b2f15df828d6820b8ef1650bf1ef123b16a56df7
empirerahul2003 <122083144+empirerahul2003@users.noreply.github.com>
2026-05-13 20:01:31 +0000

    Deploying to gh-pages from @ empirerahul2003/rahulkhanna.github.io@025f82ca6d026b84b046b1a9b8b048db8334da50 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/rahulkhanna.github.io/cv/";
@@ -174,7 +174,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=i9CjGZUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
