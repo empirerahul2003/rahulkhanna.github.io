4e2448f2501171b86b4462c6f2aec8e2b2835f4e
empirerahul2003 <122083144+empirerahul2003@users.noreply.github.com>
2026-07-01 16:19:44 +0000

    Deploying to gh-pages from @ empirerahul2003/rahulkhanna.github.io@e3426039558807dc34788eb8ebb6fc8753c7698f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -101,7 +101,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%30%33%6B%68%61%6E%6E%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
@@ -122,7 +122,7 @@ ninja.data = [{
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-scholar',
